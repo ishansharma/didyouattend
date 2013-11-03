a45ef0404f88b36e4657892f5c15d1f6c5d57a20
Ishan Sharma <i@ishan.co>
2013-11-03 23:05:34 +0530

    Added Retrieval From DB
    Values being fetched from DB now.

--- functions.php
+++ functions.php
@@ -7,7 +7,8 @@
 	$current_date = date('Y-m-d'); // current timestamp, used for saving and retrieving the values. 
 	$current_user = 1; // user currently using the app. Since multi user support is not added, 1 for now.
 	for ($i = 1; $i < 7; $i++) {
-		$sub[$i] = '';
+		$sub[$i] = 0;
+		$total[$i] = 0;
 	}
 
 	/*
@@ -37,7 +38,7 @@
 	 * Getting Data from Database
 	 */
 	function retrieve_from_db() {
-		global $current_date, $current_user;
+		global $current_date, $current_user, $total, $sub;
 		$query = mysqli_connect('127.0.0.1', 'root', '', 'dya_db');
 		if (mysqli_connect_errno($query)) {
 			echo "Failed to Connect to Database";
@@ -45,10 +46,70 @@
 		}
 		else
 		{
-			// $data_fetched = mysqli_query($query, "SELECT * FROM dya_data WHERE user_id='$current_user'");
-			// while($row = $data_fetched->fetch_array()) {
-				
-			// }
+			$data_fetched = mysqli_query($query, "SELECT * FROM dya_data WHERE user_id='$current_user'");
+			while($row = $data_fetched->fetch_array()) {
+				// print_r($row);
+				// print '<br />';
+				// Subject 1
+				if ($row['sub1'] == 1) {
+					$sub[1]++;
+					$total[1]++;
+				}
+				elseif ($row['sub1'] == 2) {
+					$total[1]++;
+				}
+
+				// Subject 2
+				if ($row['sub2'] == 1) {
+					$sub[2]++;
+					$total[2]++;
+				}
+				elseif ($row['sub2'] == 2) {
+					$total[2]++;
+				}
+
+				// Subject 3
+				if ($row['sub3'] == 1) {
+					$sub[3]++;
+					$total[3]++;
+				}
+				elseif ($row['sub3'] == 2) {
+					$total[3]++;
+				}
+
+				// Subject 4
+				if ($row['sub4'] == 1) {
+					$sub[4]++;
+					$total[4]++;
+				}
+				elseif ($row['sub4'] == 2) {
+					$total[4]++;
+				}
+
+				// Subject 5
+				if ($row['sub5'] == 1) {
+					$sub[5]++;
+					$total[5]++;
+				}
+				elseif ($row['sub5'] == 2) {
+					$total[5]++;
+				}
+
+				// Subject 6
+				if ($row['sub6'] == 1) {
+					$sub[6]++;
+					$total[6]++;
+				}
+				elseif ($row['sub6'] == 2) {
+					$total[6]++;
+				}
+			}
+			for ($i = 1; $i < 7; $i++) {
+					print $sub[$i];
+					print ' ';
+					print $total[$i];
+					print "<br />";
+				}
 		}
 	}
 ?>
